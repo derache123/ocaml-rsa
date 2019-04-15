86c0c9b0a38fa21d9dd376569a028fc01e206d32
Derek Cheng <dsc252@cornell.edu>
2019-04-14 23:11:59 -0400

    implement is_prime

--- encrypt.ml
+++ encrypt.ml
@@ -11,7 +11,13 @@ let lcm (a : int) (b : int) : int =
 
 (** [is_prime n] is true if integer [n] is prime, and false otherwise *)
 let is_prime (n : int) : bool =
-  failwith "Not yet implemented"
+  if n <= 3 then n > 1
+  else if n mod 2 = 0 || n mod 3 = 0 then false
+  else let rec loop i n =
+         if i * i > n then true
+         else if n mod i = 0 || n mod (i + 2) = 0 then false
+         else loop (i + 6) n
+    in loop 5 n
 
 (** [gen_prime m n] is a random prime number p where m <= p < n
     Requires: n > m *)
